aaccc017cc5c966eb9a2f814fc1a1e284eecf32d
Michael <admin@fillthegaplearning.co.uk>
2020-11-28 13:51:06 +0000

    Dropdown is working, populated from values from the key name, from the people array.

--- src/App.vue
+++ src/App.vue
@@ -1,39 +1,56 @@
 <template>
-  <div class="body">
-    <header class="header">Studio Ghibli API</header>
-    <div class="main-container"></div>
-      <people-list :people="people"></people-list>
-      <person-detail :person="selectedPerson" v-if="selectedPerson"></person-detail>
+  <div id ="app">
+    <h1>Studio Ghibli</h1>    
+    <!-- <div class="main-container">
+      <!-- <people-list :people='people'></people-list> -->
+      
+    <!-- </div> --> 
+    <!-- sets up a select box to pull items from the people array -->
+    <label for="person_select">Click to select a person from a film: </label>
+    <select id="person_select" v-model="selectedPerson">
+      <option disabled value=""></option>
+      <option v-for="(person, index) in people" :key="index" :value="person">{{person.name}}</option>
 
+    </select>
+    <!-- this defines person as the selected person from the select part of the web page -->
+    <person-detail :person='selectedPerson' v-if='selectedPerson'></person-detail>
   </div>
+
 </template>
 
 <script>
-import PeopleList from './components/PeopleList.vue'
 import PersonDetail from './components/PersonDetail.vue'
+// import PeopleList from './components/PeopleList.vue'
+import { eventBus } from './main.js'
+
 export default {
   name: 'app',
-  data() {
-    return {
-      ghiblis: [],
-      selectedGhibli: null,
+  data(){
+    return{
+      people: [],
+      selectedPerson: null
     };
   },
-  mounted() {
-    fetch('https://ghibliapi.herokuapp.com/people/')
-    .then(res => res.json())
-    .then(people => this.people = people)
+  components:{
+    'person-detail': PersonDetail,
+    // 'people-list': PeopleList,
 
-  
-    },
-    components: {
-      "people-list": PeopleList,
-      "person-detail": PersonDetail
-    }
+  },
+  methods(){
+
+  },
+  mounted(){
+    fetch('https://ghibliapi.herokuapp.com/people')
+    .then(response => response.json())
+    // .then(data => console.log(data))
+    // this makes the data from the of people line above = to the array 
+    .then(data => this.people = data)
+
+    eventBus.$on("person-selected", (person) => {
+      this.selectedPerson = person;
+    })
+  }
 }
-
-
-
 </script>
 
 <style>
--- src/components/PersonDetail.vue
+++ src/components/PersonDetail.vue
@@ -1,41 +1,16 @@
 <template>
-  <div v-if="person" id="personDetail">
-      <div id="film-list-wrapper">
-          <film-list v-if="film.length" :films="films"></film-list>
-      </div>
+  <div>
+      <!-- information that is important for display on the person level -->
+      <h3>{{person.name}}</h3>
+      <p>{{person.gender}}</p>
+      <p>Film to go here</p>
   </div>
-
 </template>
 
 <script>
-import FilmList from './components/FilmList.vue'
 export default {
     name: 'person-detail',
-    props: ['person'],
-    data() {
-        return {
-            films: []
-        }
-    },
-    components: {
-        'film-list': FilmList
-    },
-    mounted() {
-        this.getFilms();
-    },
-    methods: {
-        getFilms() {
-            const filmPromises = this.person.film.map((film) => {
-                return fetch(film).then(res => res.json());
-            })
-            Promise.all(filmPromises)
-            .then((data) => {
-                this.episodes = data;
-                console.log(data);
-            })
-            console.log(filmPromises);
-        }
-    }
+    props: ['selectedPerson']
 }
 </script>
 
